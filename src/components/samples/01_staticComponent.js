e18b9ddbc53ac83a2fe5055de3c0efbef1d41ae2
Claudia Marques <20095578@mail.wit.ie>
2022-06-19 17:34:03 +0100

    Start Up

---
+++ src/components/samples/01_staticComponent.js
@@ -0,0 +1,21 @@
+// 3rd party NPM modules
+// No pathname required
+import React from "react";
+
+const Demo = () => {
+  return (
+    <div>
+      <h1>Dynamic Languages</h1>
+      <ul>
+        <li>Python</li>
+        <li>Javascript</li>
+        <li>Ruby</li>
+        <li>PHP</li>
+        <li>Erlang</li>
+        <li>Clojure</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Demo;
