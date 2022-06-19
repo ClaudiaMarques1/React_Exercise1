72d73bc5bf3cd25cc5c56b9e4615811ad5aa19e8
Claudia Marques <20095578@mail.wit.ie>
2022-06-19 18:48:37 +0100

    Exercise 3 completed.

--- src/components/exercises/03_props.js
+++ src/components/exercises/03_props.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import Basic from "../../stories/exercises/03-component.stories"
 
-
-const Demo = () => {
+const Demo = (Basic) => {
   return (
-      <h1>TODO</h1>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>{Basic.course}</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr>
+          <td>{Basic.modules[0].name}</td>
+          <td>{Basic.modules[0].noLectures}</td>
+          <td>{Basic.modules[0].noPracticals}</td>
+        </tr>
+        <tr>
+          <td>{Basic.modules[1].name}</td>
+          <td>{Basic.modules[1].noLectures}</td>
+          <td>{Basic.modules[1].noPracticals}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
